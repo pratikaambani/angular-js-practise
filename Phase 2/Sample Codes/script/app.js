176aca412dad06afd28d5163e05a243f5a0e0961
Pratik <pratikaambani@gmail.com>
2020-01-13 07:21:59 +0530

    adds notes

---
+++ Phase 2/Sample Codes/script/app.js
@@ -0,0 +1,35 @@
+//001
+var sample = angular.module("ContactApp", []);
+sample.controller("ContactCtrl", SampleFunction);
+
+function SampleFunction($scope) {
+	$scope.jsonObjectData = {
+	color: "red",
+	value: "#f00"
+	}
+
+	$scope.anotherJsonObjectData = {
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+
+
+}
---
+++ Phase 2/Sample Codes/script/data.js
@@ -0,0 +1,8 @@
+//not used
+//001
+var sample = angular.module("ContactApp", []);
+sample.controller("ContactCtrl", SampleFunction);
+
+function SampleFunction() {
+	this.userDetails = {"name": "pratik"};
+}
